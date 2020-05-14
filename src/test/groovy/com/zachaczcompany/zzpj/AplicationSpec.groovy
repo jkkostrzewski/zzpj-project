5cf5a682c78ca129517921e9b91a2c906311bcbf
jkkostrzewski <jakub.kostrzewski0@gmail.com>
2020-05-14 18:25:04 +0200

    Initial project
    Added groovy + spock, h2 database dependencies. Maven set up for running tests with every build.

---
+++ src/test/groovy/com/zachaczcompany/zzpj/AplicationSpec.groovy
@@ -0,0 +1,10 @@
+package com.zachaczcompany.zzpj
+
+import spock.lang.Specification
+
+class AplicationSpec extends Specification {
+    def "one plus one should equal two"() {
+        expect:
+        1 + 1 == 2
+    }
+}
