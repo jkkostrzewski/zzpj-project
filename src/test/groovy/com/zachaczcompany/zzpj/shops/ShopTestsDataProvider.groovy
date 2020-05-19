4e184fa5d47f531ba549f8ab921bc97997cba745
jkkostrzewski <jakub.kostrzewski0@gmail.com>
2020-05-19 19:23:19 +0200

    add shop test helper methods

---
+++ src/test/groovy/com/zachaczcompany/zzpj/shops/ShopTestsDataProvider.groovy
@@ -0,0 +1,51 @@
+package com.zachaczcompany.zzpj.shops
+
+import com.zachaczcompany.zzpj.shops.persistence.Address
+import com.zachaczcompany.zzpj.shops.persistence.DailyOpenHours
+import com.zachaczcompany.zzpj.shops.persistence.Localization
+import com.zachaczcompany.zzpj.shops.persistence.OpenHours
+import com.zachaczcompany.zzpj.shops.persistence.Shop
+import com.zachaczcompany.zzpj.shops.persistence.ShopDetails
+import com.zachaczcompany.zzpj.shops.persistence.ShopStats
+import com.zachaczcompany.zzpj.shops.persistence.StockType
+
+import java.time.DayOfWeek
+import java.time.LocalTime
+
+class ShopTestsDataProvider {
+    static Address anyAddress() {
+        new Address("Warsaw", "Street", 15, "230", "05-610")
+    }
+
+    static DailyOpenHours anyDailyOpenHours() {
+        new DailyOpenHours(DayOfWeek.FRIDAY, LocalTime.NOON, LocalTime.MIDNIGHT)
+    }
+
+    static Localization anyLocalization() {
+        new Localization(-62.78471, 49.12860)
+    }
+
+    static Set<DailyOpenHours> anyDailyOpenHoursSet() {
+        Set<DailyOpenHours> openHours = new HashSet<>();
+        Arrays.stream(DayOfWeek.values()).forEach(day -> {
+            openHours.add(new DailyOpenHours(day, LocalTime.NOON, LocalTime.MIDNIGHT))
+        })
+        return openHours
+    }
+
+    static OpenHours anyOpenHours() {
+        new OpenHours(anyDailyOpenHoursSet())
+    }
+
+    static ShopDetails anyShopDetails() {
+        new ShopDetails(StockType.DETERGENTS, anyLocalization(), anyOpenHours())
+    }
+
+    static ShopStats anyShopStats() {
+        new ShopStats(13, 13, 4)
+    }
+
+    static Shop anyShop() {
+        new Shop("Leadl", anyAddress(), anyShopDetails(), anyShopStats())
+    }
+}
