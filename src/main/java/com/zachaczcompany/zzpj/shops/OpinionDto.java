72a21b6ec599a60da63c33c6ad49056abe7921a4
Paula Zachacz <paula.zachacz@onet.pl>
2020-06-14 21:51:59 +0200

    Size constraints added

--- src/main/java/com/zachaczcompany/zzpj/shops/OpinionDto.java
+++ src/main/java/com/zachaczcompany/zzpj/shops/OpinionDto.java
@@ -3,10 +3,15 @@ package com.zachaczcompany.zzpj.shops;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
 @AllArgsConstructor
 @Getter
 public class OpinionDto {
     private Long shopId;
+    @Min(0)
+    @Max(5)
     private Integer rate;
     private String description;
 }
