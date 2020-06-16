0359c3ce0c7e9864caaf52e7d5ae67bf56d79bd5
jkkostrzewski <45603875+jkkostrzewski@users.noreply.github.com>
2020-06-16 02:09:02 +0200

    Add swagger together with description adnotations in controllers and dto (#36)
    Remove unused shopFacade in RegistrationController
    Rename variables in StatisticsUpdateDto

--- src/main/java/com/zachaczcompany/zzpj/security/jwt/RegistrationController.java
+++ src/main/java/com/zachaczcompany/zzpj/security/jwt/RegistrationController.java
@@ -2,6 +2,11 @@ package com.zachaczcompany.zzpj.security.jwt;
 
 import com.zachaczcompany.zzpj.security.OwnerSignUpDto;
 import com.zachaczcompany.zzpj.shops.domain.ShopFacade;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,18 +19,27 @@ import javax.validation.Valid;
 @RequestMapping("/register")
 public class RegistrationController {
     private final UserRegistrationService userRegistrationService;
-    private final ShopFacade shopFacade;
 
-    public RegistrationController(UserRegistrationService userRegistrationService, ShopFacade shopFacade) {
+    public RegistrationController(UserRegistrationService userRegistrationService) {
         this.userRegistrationService = userRegistrationService;
-        this.shopFacade = shopFacade;
     }
 
+    @Operation(summary = "Register new owner account", description = "Requires owner credentials and new shop information")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful sign up",
+                    content = @Content(schema = @Schema(implementation = OwnerSignUpDto.class))),
+            @ApiResponse(responseCode = "400", description = "Username has already been used")
+    })
     @PostMapping("/owner")
     public ResponseEntity signUpOwner(@RequestBody @Valid OwnerSignUpDto ownerSignUpDto) {
         return userRegistrationService.registerOwner(ownerSignUpDto).toResponseEntity();
     }
 
+    @Operation(summary = "Register new shop employee", description = "Requires username and password")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful sign up"),
+            @ApiResponse(responseCode = "400", description = "Username has already been used")
+    })
     @PostMapping("/employee")
     public ResponseEntity signUpEmployee(@RequestBody @Valid UserSignUp userSignUp) {
         return userRegistrationService.registerEmployee(userSignUp).toResponseEntity();
--- src/main/java/com/zachaczcompany/zzpj/security/jwt/UserSignUp.java
+++ src/main/java/com/zachaczcompany/zzpj/security/jwt/UserSignUp.java
@@ -1,5 +1,6 @@
 package com.zachaczcompany.zzpj.security.jwt;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,9 +13,11 @@ import javax.validation.constraints.NotEmpty;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserSignUp {
+    @Schema(example = "newUser123")
     @NotEmpty
     private String username;
 
+    @Schema(example = "#!newPassword123")
     @NotEmpty
     private String password;
 }
